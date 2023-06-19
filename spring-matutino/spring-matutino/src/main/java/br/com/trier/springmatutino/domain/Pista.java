da36316b558471d4343409dbb6d9275a412ea284
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2023-06-19 18:50:35 -0300

    Delete spring-matutino/spring-matutino directory

--- spring-matutino/spring-matutino/src/main/java/br/com/trier/springmatutino/domain/Pista.java
+++
@@ -1,5 +0,0 @@
-package br.com.trier.springmatutino.domain;
-
-public class Pista {
-
-}
--- spring-matutino/spring-matutino/src/main/java/br/com/trier/springmatutino/services/exceptions/ViolacaoIntegridade.java
+++
@@ -1,9 +0,0 @@
-package br.com.trier.springmatutino.services.exceptions;
-
-@SuppressWarnings("serial")
-public class ViolacaoIntegridade extends RuntimeException{
-
-	public ViolacaoIntegridade(String mensagem) {
-		super(mensagem);
-	}
-}
